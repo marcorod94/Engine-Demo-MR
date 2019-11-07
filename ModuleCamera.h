5133b98ec4cdbed2610e7f90b049582b2bf2ed8a
marcorod94 <marco.rod.94@live.com>
2019-11-07 21:21:26 +0100

    Axis and Ground
    Showing the x, y and z axis and the ground using camera module

--- ModuleCamera.h
+++ ModuleCamera.h
@@ -1,8 +1,17 @@
-#pragma once
-class ModuleCamera
-{
+#ifndef ModuleCamera_h
+#define ModuleCamera_h
+
+#include "Module.h"
+#include "MathGeoLib.h"
+
+class ModuleCamera : public Module {
 public:
 	ModuleCamera();
 	~ModuleCamera();
+	bool Init();
+	float4x4 LookAt(float3, float3, float3);
+public:
+	Frustum frustum;
+	float4x4 proj, view, model;
 };
-
+#endif
